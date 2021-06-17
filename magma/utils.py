1be810ec544b0747b001502758f8a55759360515
Manu Flores <emanuelflrsb@hotmail.com>
2021-06-16 22:00:48 -0700

    fixed function one_vs_all

--- magma/utils.py
+++ magma/utils.py
@@ -1310,13 +1310,14 @@ def get_cosine_distribution_drug(drugbank, adata, query_drug_name, perturb_drug_
     return cosine_similarity_distribution
 
 
-def get_cosine_drug_one_vs_all(drug_name, cosine_arr):
+def get_cosine_drug_one_vs_all(drugbank, adata, drug_name, cosine_arr):
     """
     Returns the cosine similarity distribution of a molecule with cells perturbed by it,
     and the cos. sim. dist. of the molecule with cells coming from other samples.
+    
     """
     n_mols, n_cells = cosine_arr.shape
-    ix_drug, ix_cells = get_ix_drug(drug_name), get_ix_cells(drug_name)
+    ix_drug, ix_cells = get_ix_drug(drugbank, drug_name), get_ix_cells(drugbank, drug_name)
 
     # Get cosine similarity distribution of a drug with itself
     cosine_cells_drug = cosine_arr[ix_drug, ix_cells]
