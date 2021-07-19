a69ab8ae76f23025c02909190db78ff74be3b235
Manu Flores <emanuelflrsb@hotmail.com>
2021-07-19 13:57:31 -0700

    setting up log exporting

--- magma/utils.py
+++ magma/utils.py
@@ -1094,27 +1094,27 @@ class JointEmbeddingTrainer:
         df_train_logs = pd.concat([df_train_loss, df_train_acc], axis = 1)
         df_test_logs = pd.concat([df_test_loss, df_test_acc], axis = 1)
 
-        epoch_indicator_train = np.concatenate(
-            [np.repeat(epoch, self.n_train_batches) for epoch in np.arange(1, self.n_epochs+1)]
-        )
-
-        epoch_indicator_test = np.concatenate(
-            [np.repeat(epoch, self.n_test_batches) for epoch in np.arange(1, self.n_epochs +1)]
-        )
-
-        df_train_logs['epoch'] = epoch_indicator_train
-        df_test_logs['epoch'] = epoch_indicator_test
+        # epoch_indicator_train = np.concatenate(
+        #     [np.repeat(epoch, self.n_train_batches) for epoch in np.arange(1, self.n_epochs+1)]
+        # )
+        #
+        # epoch_indicator_test = np.concatenate(
+        #     [np.repeat(epoch, self.n_test_batches) for epoch in np.arange(1, self.n_epochs +1)]
+        # )
+        #
+        # df_train_logs['epoch'] = epoch_indicator_train
+        # df_test_logs['epoch'] = epoch_indicator_test
 
         # Set logs as attributes
         self.train_logs = df_train_logs
         self.test_logs = df_test_logs
 
-        df_train_agg = df_train_logs.groupby('epoch').mean()
-        df_test_agg = df_test_logs.groupby('epoch').mean()
+        #df_train_agg = df_train_logs.groupby('epoch').mean()
+        #df_test_agg = df_test_logs.groupby('epoch').mean()
 
-        self.best_model_ix = df_test_agg.test_acc.argmax()
+        #self.best_model_ix = df_test_agg.test_acc.argmax()
 
-        return df_train_agg, df_test_agg
+        return df_train_logs, df_test_logs #df_train_agg, df_test_agg
 
 
 def train_vae(
