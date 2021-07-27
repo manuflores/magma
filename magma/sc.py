9450267fa75dc2b5a5ee69177673752c0f4f2813
Manu Flores <emanuelflrsb@hotmail.com>
2021-07-26 20:58:18 -0600

    debugging deg

--- magma/sc.py
+++ magma/sc.py
@@ -49,7 +49,7 @@ def get_scanpy_deg_report_df(
     # Record information for each group / cluster in the report df
     for g in groups:
         df = pd.DataFrame(
-                np.vstack(([deg_result_dict[col][g] for col in cols_annot])).T,
+                np.concatenate([deg_result_dict[col][g] for col in cols_annot], axis = 1),
             columns=["gene_name", "log_fc", "pval_adj"],
         )
 
