b622a4b99416fc634a78bef46fc7b52bf24698b8
Manu Flores <emanuelflrsb@hotmail.com>
2022-02-17 00:01:54 -0800

    fixed n categories...

--- magma/utils.py
+++ magma/utils.py
@@ -2753,7 +2753,7 @@ class adata_torch_dataset(Dataset):
             enc = OneHotEncoder(sparse = False)
             self.one_hot_encoder = enc
 
-            n_categories = len(self.data["target_col"].unique())
+            n_categories = len(self.data.obs["target_col"].unique())
 
             # Extract target data
             y_data = self.data.obs[self.target_col].values.astype(str).reshape(-1, n_categories)
